1962c78ae5937e4d1f6d131262d22c07384c8f9e
Julius Makel - I20 <julius.makel@nordakademie.de>
2023-09-03 18:15:02 +0200

    Add favicon

--- worker.js
+++ worker.js
@@ -1,16 +1,7 @@
 import init, { initThreadPool, mandelbrot, mandelbrot_parallel } from "./pkg/mandelbrot.js";
 
-console.log("Worker initialized");
-console.log("Worker initialized");
 await init();
-console.log("Worker initialized");
-init().then(() => {
-	console.log("initted");
-}).catch(e => {
-	console.log(e)
-});
 await initThreadPool(navigator.hardwareConcurrency);
-console.log("threadpooled");
 
 onmessage = (message) => {
 	let [width, height, scale, iterations, centerX, centerY, parallel] = message.data;
